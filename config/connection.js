f125a1b8334d16e93033c77306f45394056c3c40
Nackles <45973617+Nackles@users.noreply.github.com>
2019-04-11 15:06:38 -0700

    heroku function :^)

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 require('dotenv').config()
-
 var mysql = require('mysql')
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: 'burgers_db'
-})
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: 'burgers_db'
+    })
+}
 
 connection.connect(function (err) {
     if (err) throw err
